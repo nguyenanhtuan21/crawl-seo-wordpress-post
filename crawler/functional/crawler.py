b5f182afad18b974d096b9d0a8038ec93017b49a
vumanhcuongUET <mdc011645996@gmail.com>
2022-10-14 14:09:32 +0700

    Update fix timeout, 404 code

--- crawler/functional/crawler.py
+++ crawler/functional/crawler.py
@@ -18,125 +18,164 @@ def is_external_link(url):
 
 class Crawler:
     def __init__(self, url):
-        pass
-        html_text = requests.get(url)
-        self.html_response = BeautifulSoup(html_text.text, 'lxml')
+        try:
+            html_text = requests.get(url)
+            self.html_response = BeautifulSoup(html_text.text, 'lxml')
+        except requests.exceptions.Timeout:
+            self.html_response = "TIME OUT"
 
     def get_meta_title(self):
-        return self.html_response.title.string
+        if self.html_response == "TIME OUT" :
+            result = "No Data"
+        else:
+            result = self.html_response.title.string
+        return result
 
     def get_author(self):
-        lst_author = self.html_response.find_all('meta', {'name': 'author'})
-        author = lst_author[0]['content']
-        return author
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            lst_author = self.html_response.find_all('meta', {'name': 'author'})
+            author = lst_author[0]['content']
+            return author
 
     def get_list_category(self):
-        lst_category = self.html_response.find_all('a', {'class': 'entry-crumb'})
         lst_category_name = []
-        for cat in lst_category:
-            lst_category_name.append(cat.get_text())
+        if self.html_response == "TIME OUT":
+            lst_category_name = []
+        else:
+            lst_category = self.html_response.find_all('a', {'class': 'entry-crumb'})
+            for cat in lst_category:
+                lst_category_name.append(cat.get_text())
+
         return lst_category_name
 
     def get_url(self):
-        url_tag = self.html_response.find('meta', {'property': 'og:url'})
-        return url_tag['content']
+       if self.html_response == "TIME OUT":
+           return "No Data"
+       else:
+           url_tag = self.html_response.find('meta', {'property': 'og:url'})
+           return url_tag['content']
 
     def get_num_word(self):
-        content_post = self.html_response.find('div', {'class': 'td-post-content'})
-        num_word = len(content_post.text.split())
-        return num_word
+        if self.html_response == "TIME OUT":
+            return 0
+        else:
+            content_post = self.html_response.find('div', {'class': 'td-post-content'})
+            num_word = len(content_post.text.split())
+            return num_word
 
     def get_internal_link_list(self):
-        pass
-        internal_a_list = self.html_response.find_all('a', href=True)
-        links = []
-        for a_tag in internal_a_list:
-            links.append(a_tag['href'])
-        # filter internal link
-        internal_links = []
-        for link in links:
-            if is_internal_link(link):
-                internal_links.append(link)
+        if self.html_response == "TIME OUT":
+            return []
+        else:
+            internal_a_list = self.html_response.find_all('a', href=True)
+            links = []
+            for a_tag in internal_a_list:
+                links.append(a_tag['href'])
+            # filter internal link
+            internal_links = []
+            for link in links:
+                if is_internal_link(link):
+                    internal_links.append(link)
 
-        return internal_links
+            return internal_links
 
     def get_external_link_list(self):
-        pass
-        external_a_list = self.html_response.find_all('a', href=True)
-        links = []
-        for a_tag in external_a_list:
-            links.append(a_tag['href'])
-        # filter external link
-        external_links = []
-        for link in links:
-            if is_external_link(link):
-                external_links.append(link)
+        if self.html_response == "TIME OUT":
+            return []
+        else:
+            external_a_list = self.html_response.find_all('a', href=True)
+            links = []
+            for a_tag in external_a_list:
+                links.append(a_tag['href'])
+            # filter external link
+            external_links = []
+            for link in links:
+                if is_external_link(link):
+                    external_links.append(link)
 
-        return external_links
+            return external_links
 
     def get_tag_list(self):
-        pass
-        tag_tag_list = self.html_response.select(".td-tags a")
-        tags = []
-        if len(tag_tag_list) > 0:
-            for tag in tag_tag_list:
-                tags.append(tag.text)
-            return tags
-        else:
+        if self.html_response == "TIME OUT":
             return ""
+        else:
+            tag_tag_list = self.html_response.select(".td-tags a")
+            tags = []
+            if len(tag_tag_list) > 0:
+                for tag in tag_tag_list:
+                    tags.append(tag.text)
+                return tags
+            else:
+                return ""
 
     def index_or_no(self):
-        pass
-        meta_robots = self.html_response.find_all('meta', {'name': ['robots', 'googlebot', 'bingbot']})
-        for meta in meta_robots:
-            # print(meta['content'])
-            if meta['content'].find('noindex') >= 0:
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            meta_robots = self.html_response.find_all('meta', {'name': ['robots', 'googlebot', 'bingbot']})
+            for meta in meta_robots:
                 # print(meta['content'])
-                return 'noindex'
-        return 'index'
+                if meta['content'].find('noindex') >= 0:
+                    # print(meta['content'])
+                    return 'noindex'
+            return 'index'
 
     def follow_or_no(self):
-        pass
-        meta_robots = self.html_response.find_all('meta', {'name': ['robots', 'googlebot', 'bingbot']})
-        for meta in meta_robots:
-            # print(meta['content'])
-            if meta['content'].find('nofollow') >= 0:
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            meta_robots = self.html_response.find_all('meta', {'name': ['robots', 'googlebot', 'bingbot']})
+            for meta in meta_robots:
                 # print(meta['content'])
-                return 'nofollow'
-        return 'follow'
+                if meta['content'].find('nofollow') >= 0:
+                    # print(meta['content'])
+                    return 'nofollow'
+            return 'follow'
 
     def get_hreflang(self):
-        pass
-        link_tags = self.html_response.find('link', {'hreflang': True})
-        # for link_tag in link_tags:
-        return link_tags['hreflang']
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            link_tags = self.html_response.find('link', {'hreflang': True})
+            # for link_tag in link_tags:
+            return link_tags['hreflang']
 
     def get_meta_description(self):
-        pass
-        meta_descriptions = self.html_response.find('meta', {'name': 'description'})
-        meta_des = meta_descriptions['content']
-        return meta_des
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            meta_descriptions = self.html_response.find('meta', {'name': 'description'})
+            meta_des = meta_descriptions['content']
+            return meta_des
 
     def get_canonical_link(self):
-        pass
-        canonical_tag = self.html_response.find('link', {'rel': 'canonical'})
-        return canonical_tag['href']
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            canonical_tag = self.html_response.find('link', {'rel': 'canonical'})
+            return canonical_tag['href']
 
     def get_date_published(self):
-        pass
-        lst_published = self.html_response.find_all('meta', {'property': 'article:published_time'})
-        date_published = lst_published[0]['content']
-        return date_published
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            lst_published = self.html_response.find_all('meta', {'property': 'article:published_time'})
+            date_published = lst_published[0]['content']
+            return date_published
 
     def get_date_modified(self):
-        pass
-        lst_modified = self.html_response.find_all('meta', {'property': 'article:modified_time'})
-        lst_modified1 = self.html_response.find_all('meta', {'itemprop': 'datePublished'})
-        if(len(lst_modified) > 0):
-            date_modified = lst_modified[0]['content']
-        if(len(lst_modified1) > 0):
-            date_modified = lst_modified1[0]['content']
-        return date_modified
+        if self.html_response == "TIME OUT":
+            return "No Data"
+        else:
+            lst_modified = self.html_response.find_all('meta', {'property': 'article:modified_time'})
+            lst_modified1 = self.html_response.find_all('meta', {'itemprop': 'datePublished'})
+            if(len(lst_modified) > 0):
+                date_modified = lst_modified[0]['content']
+            if(len(lst_modified1) > 0):
+                date_modified = lst_modified1[0]['content']
+            return date_modified
 
     @staticmethod
     def list_to_string(s):
--- crawler/main.py
+++ crawler/main.py
@@ -6,14 +6,16 @@ import threading
 
 
 def open_file(path):
-    f = open(path, "r")
     list_crawler = []
-    for idData in f:
-        print(idData.strip())
-        if idData != '':
-            crawler = Crawler("https://amis.misa.vn/" + idData.strip())
+    with open(path, 'r') as f:
+        for idData in f:
+            if "\n" in idData:
+                id_url = idData.rstrip('\n')
+            else:
+                id_url = idData
+            print(id_url)
+            crawler = Crawler("https://amis.misa.vn/" + id_url)
             list_crawler.append(crawler)
-
     return list_crawler
 
 
@@ -24,77 +26,81 @@ def export_excel(path, i):
                     "Danh sách External Link trong bài", "Tag", "Index/NoIndex", "Follow/Nofollow", "Ahref-Lang",
                     "Meta Title", "Meta Description", "Meta keyword", "Canonical", "Date Published", "Date Modified"]
     list_crawl = open_file(path)
+    time.sleep(0.1)
     index = 1
     for header in list_headers:
         column_header = my_sheet.cell(row=1, column=list_headers.index(header) + 1)
         column_header.value = header
 
     for idCrawler in list_crawl:
-        index += 1
-        # Tạo cột số thứ tự
-        data = my_sheet.cell(row=index, column=1)
-        data.value = index - 1
-        # Tạo cột category
-        data = my_sheet.cell(row=index, column=2)
-        data.value = idCrawler.list_to_string(idCrawler.get_list_category())
-        # Tạo cột author
-        author_data = my_sheet.cell(row=index, column=3)
-        author_data.value = idCrawler.get_author()
-        # Tạo cột url
-        url_data = my_sheet.cell(row=index, column=4)
-        url_data.value = idCrawler.get_url()
+        if idCrawler.get_meta_description() == "404 - MISA AMIS":
+            print("Can't crawl Data")
+        else:
+            index += 1
+            # Tạo cột số thứ tự
+            data = my_sheet.cell(row=index, column=1)
+            data.value = index - 1
+            # Tạo cột category
+            data = my_sheet.cell(row=index, column=2)
+            data.value = idCrawler.list_to_string(idCrawler.get_list_category())
+            # Tạo cột author
+            author_data = my_sheet.cell(row=index, column=3)
+            author_data.value = idCrawler.get_author()
+            # Tạo cột url
+            url_data = my_sheet.cell(row=index, column=4)
+            url_data.value = idCrawler.get_url()
 
-        # Tạo cột số lượng dữ liệu
-        number_of_data = my_sheet.cell(row=index, column=5)
-        number_of_data.value = idCrawler.get_num_word()
+            # Tạo cột số lượng dữ liệu
+            number_of_data = my_sheet.cell(row=index, column=5)
+            number_of_data.value = idCrawler.get_num_word()
 
-        # Tạo cột internalUrl
+            # Tạo cột internalUrl
 
-        internal_data = my_sheet.cell(row=index, column=6)
-        internal_data.value = idCrawler.list_to_string(idCrawler.get_internal_link_list())
+            internal_data = my_sheet.cell(row=index, column=6)
+            internal_data.value = idCrawler.list_to_string(idCrawler.get_internal_link_list())
 
-        # Tạo cột externalUrl
+            # Tạo cột externalUrl
 
-        external_data = my_sheet.cell(row=index, column=7)
-        external_data.value = idCrawler.list_to_string(idCrawler.get_external_link_list())
+            external_data = my_sheet.cell(row=index, column=7)
+            external_data.value = idCrawler.list_to_string(idCrawler.get_external_link_list())
 
-        # Tạo cột tag
-        tag_list_data = my_sheet.cell(row=index, column=8)
-        tag_list_data.value = ','.join(str(tagData) for tagData in idCrawler.get_tag_list())
+            # Tạo cột tag
+            tag_list_data = my_sheet.cell(row=index, column=8)
+            tag_list_data.value = ','.join(str(tagData) for tagData in idCrawler.get_tag_list())
 
-        # Tạo cột xác định index hay noindex
-        is_index_data = my_sheet.cell(row=index, column=9)
-        is_index_data.value = idCrawler.index_or_no()
+            # Tạo cột xác định index hay noindex
+            is_index_data = my_sheet.cell(row=index, column=9)
+            is_index_data.value = idCrawler.index_or_no()
 
-        # Tạo cột xác định folow hay noFollow
-        is_follow_data = my_sheet.cell(row=index, column=10)
-        is_follow_data.value = idCrawler.follow_or_no()
+            # Tạo cột xác định folow hay noFollow
+            is_follow_data = my_sheet.cell(row=index, column=10)
+            is_follow_data.value = idCrawler.follow_or_no()
 
-        # Tạo cột hrefLanguage
-        href_lang_data = my_sheet.cell(row=index, column=11)
-        href_lang_data.value = idCrawler.get_hreflang()
+            # Tạo cột hrefLanguage
+            href_lang_data = my_sheet.cell(row=index, column=11)
+            href_lang_data.value = idCrawler.get_hreflang()
 
-        # Tạo cột meta title
-        meta_title_data = my_sheet.cell(row=index, column=12)
-        meta_title_data.value = str(idCrawler.get_meta_title())
+            # Tạo cột meta title
+            meta_title_data = my_sheet.cell(row=index, column=12)
+            meta_title_data.value = str(idCrawler.get_meta_title())
 
-        # Tạo cột meta description
-        meta_description_data = my_sheet.cell(row=index, column=13)
-        meta_description_data.value = idCrawler.get_meta_description()
+            # Tạo cột meta description
+            meta_description_data = my_sheet.cell(row=index, column=13)
+            meta_description_data.value = idCrawler.get_meta_description()
 
-        # Tạo cột canonicalUrl
-        meta_canonical_data = my_sheet.cell(row=index, column=15)
-        meta_canonical_data.value = idCrawler.get_canonical_link()
+            # Tạo cột canonicalUrl
+            meta_canonical_data = my_sheet.cell(row=index, column=15)
+            meta_canonical_data.value = idCrawler.get_canonical_link()
 
-        # Tạo cột date published
-        date_publish_data = my_sheet.cell(row=index, column=16)
-        date_publish_data.value = idCrawler.get_date_published()
+            # Tạo cột date published
+            date_publish_data = my_sheet.cell(row=index, column=16)
+            date_publish_data.value = idCrawler.get_date_published()
 
-        # Tạo cọt date modified
-        date_modified_data = my_sheet.cell(row=index, column=17)
-        date_modified_data.value = idCrawler.get_date_modified()
-        print(index - 1)
-        my_wb.save("crawl_data" + i + ".xlsx")
+            # Tạo cọt date modified
+            date_modified_data = my_sheet.cell(row=index, column=17)
+            date_modified_data.value = idCrawler.get_date_modified()
+            print(index - 1)
+            my_wb.save("crawl_data" + i + ".xlsx")
 
 
 # main function
